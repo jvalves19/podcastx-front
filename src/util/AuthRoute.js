1a35b79cdee2c94a1ea962ed2fcd13c5aa22b518
jvalves19 <jvalves19@outlook.com>
2021-06-22 16:26:02 -0300

    Pinto

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -3,9 +3,9 @@ import { Route, Redirect } from 'react-router-dom';
 
 const AuthRoute = (props) => {
   // ? e : significam IfElse (if true (?) então <Redirect> Senão(:) <Component>)
-  const authenticated = !!localStorage.getItem('FBIdToken')
+  const authenticated = !!localStorage.getItem('FBIdToken');
 
-  return !(authenticated) ? <Route {...props} /> : <Redirect to='/' />
+  return (authenticated) ? <Redirect to='/' /> : <Route {...props} />
 };
 
 export default AuthRoute;
